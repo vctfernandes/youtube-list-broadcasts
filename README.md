61beef3c495560d7ecdb1678032afafd573f3588
vctfernades <vct.fernandes@gmail.com>
2016-01-19 17:02:07 -0200

    Fixed the getNewToken part

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # youtube-list-broadcasts
-NodeJS module meant to list all of your active broadcasts. YouTube API v3 compliant.
+NodeJS module meant to list all of your active broadcasts and say if the specified id is actively broadcasting. YouTube API v3 compliant.
+
+Here is what I'm doing outside the module to talk with it:
+    var express = require('express');
+    var youtube = require('youtube-list-broadcasts')
+    var mongojs = require('mongojs')
+    var db = mongojs('mydb', ['mycollection'])
+    var app = express();
+    var settings = {
+			client_id: CLIENT_ID,
+			client_secret: CLIENT_SECRET,
+			redirect_url: "https://www.mywebsite.com/callback/youtube",
+			collection: db.mycollection
+		}
+
+    app.get('/', function(req, res){
+      youtube.liveBroadcasts(YOUTUBE_CODE, settings, function(err, response) {
+        if(err) {
+          res.send('Move along... ' + err)	
+        } else {
+          res.send("It's  alive!!");
+        }
+      })
+    })
+
+    app.get('/callback/youtube', function(req, res) {
+      youtube.getNewToken(settings, req.query.code, function(err, result) {
+        res.redirect('/')
+      })
+    })
+
+    app.listen(80);
--- library.js
+++ library.js
@@ -9,40 +9,49 @@ exports.liveBroadcasts = function(id, clientInfo, callback) {
 	collection = clientInfo.collection
 	isLive = false;
 	count = 0;
+	needsNewToken = false
 	async.whilst(
 		function() {
-			return (!isLive && count < MAX_TRIES) 
+			return (!isLive && count < MAX_TRIES && !needsNewToken) 
 		},
 		function(callback) {
 			count++
 			setTimeout(
-				authorize(clientInfo, collection, function(oauth2Client) {
-					listLiveBroadcasts(oauth2Client, id, function(err, res) {
-						if(err) {
-							console.log("YOUTUBE --- Refreshing the token.")
-							refreshToken(oauth2Client, collection, function() {
-								listLiveBroadcasts(oauth2Client, id, function(err, res) {
-									if(err) {
-										callback(true, null)
-									} else {
-										if(findId(res.items, id)) {
-											isLive = true	
+				authorize(clientInfo, collection, function(err, oauth2Client) {
+					if(err){
+						console.log(err)
+						needsNewToken = true
+						callback(needsNewToken, null)
+					} else {
+						listLiveBroadcasts(oauth2Client, id, function(err, res) {
+							if(err) {
+								console.log("YOUTUBE --- Refreshing the token.")
+								refreshToken(oauth2Client, collection, function() {
+									listLiveBroadcasts(oauth2Client, id, function(err, res) {
+										if(err) {
+											callback(true, null)
+										} else {
+											if(findId(res.items, id)) {
+												isLive = true	
+											}
+											callback(null, res.items)	
 										}
-										callback(null, res.items)	
-									}
+									})
 								})
-							})
-						} else {
-							if(findId(res.items, id)) {
-								isLive = true	
+							} else {
+								if(findId(res.items, id)) {
+									isLive = true	
+								}
+								callback(null, res.items)	
 							}
-							callback(null, res.items)	
-						}
-					})
+						})	
+					}
 				}), DELAY)
 		}, function (err, results) {
 			if(count >= MAX_TRIES) {
 				callback('timeout', null)
+			} else if (needsNewToken) {
+				callback('needsNewToken',null)
 			} else {
 				callback(null, results)
 			}
@@ -68,37 +77,38 @@ function authorize(credentials, collection, callback) {
 
 	collection.findOne({}, function(err, token) {
 		if(!token || typeof token.access_token === 'undefined' || token.access_token == '') {
-			getNewToken(oauth2Client, collection, callback)
+			getNewCode(oauth2Client, collection, function(err, result) {
+				callback(err, result)
+			})
 		} else {
 			oauth2Client.credentials = token
-			callback(oauth2Client)
+			callback(err, oauth2Client)
 		}
 	})
 }
 
-function getNewToken(oauth2Client, collection, callback) {
+function getNewCode(oauth2Client, collection, callback) {
 	var authUrl = oauth2Client.generateAuthUrl({
 		access_type: 'offline',
 		scope: ["https://www.googleapis.com/auth/youtube.readonly"]
 	})
-	console.log('YOUTUBE --- Authorize this app by visiting this URL: ', authUrl + '&approval_prompt=force')
-	var rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	})
-	rl.question('YOUTUBE --- Enter the code from that page here: ', function(code) {
-		rl.close()
-		oauth2Client.getToken(code, function(err, token) {
-			if (err) {
-				console.log('YOUTUBE --- ' + err)
-				callback()
-			} else {
-				oauth2Client.credentials = token
-				storeToken(collection, token, function() {
-					callback(oauth2Client)
-				})
-			}
-		})
+	console.log('Authorize this app by visiting this URL: ' + authUrl + '&approval_prompt=force')
+	callback('needsNewToken', null)
+}
+
+exports.getNewToken = function(settings, code, callback) {
+	var auth = new googleAuth()
+	var oauth2Client = new auth.OAuth2(settings.client_id, settings.client_secret, settings.redirect_url)
+	oauth2Client.getToken(code, function(err, token) {
+		if (err) {
+			console.log('YOUTUBE --- ' + err)
+			callback(err, null)
+		} else {
+			oauth2Client.credentials = token
+			storeToken(settings.collection, token, function() {
+				callback(null, 'ok')
+			})
+		}
 	})
 }
 
